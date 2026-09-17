7029974f6bce08dc05e1ca779c6d28e29109db94
bhargav24bcy10224-wq <bhargav.24bcy10224@vitbhopal.ac.in>
2026-09-17 23:53:39 +0530

    Update Account.java

--- src/com/bank/model/Account.java
+++ src/com/bank/model/Account.java
@@ -1,1 +1,24 @@
-package com.bank.model; import java.io.Serializable; public abstract class Account implements Serializable { protected String accountNumber; protected String holderName; protected double balance; public Account(String acc, String name, double bal) { this.accountNumber = acc; this.holderName = name; this.balance = bal; } public abstract void withdraw(double amt) throws Exception; public void deposit(double amt) { if(amt > 0) balance += amt; } public String getAccountNumber() { return accountNumber; } public String getHolderName() { return holderName; } public double getBalance() { return balance; } }
+package com.bank.model; 
+import java.io.Serializable; 
+public abstract class Account implements Serializable { 
+  protected String accountNumber; 
+  protected String holderName; 
+  protected double balance; 
+  
+  public Account(String acc, String name, double bal) { 
+    this.accountNumber = acc; 
+    this.holderName = name; 
+    this.balance = bal; 
+  } 
+  
+  public abstract void withdraw(double amt) throws Exception; 
+  
+  public void deposit(double amt) { 
+    if(amt > 0) balance += amt; 
+  } 
+  
+  public String getAccountNumber() { return accountNumber; } 
+  
+  public String getHolderName() { return holderName; } 
+  
+  public double getBalance() { return balance; } }
